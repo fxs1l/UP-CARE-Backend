d964dae74299baddcb366cb2c3fd7b689d68cce2
Asil Andrei Acasio <fx.silndrei@gmail.com>
2025-03-27 22:26:44 +0800

    chore: code cleanup on thread address

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,7 @@ import server from "./app";
 import log from "@/utils/logging";
 
 const port = process.env.PORT ? parseInt(process.env.PORT, 10) : 5683;
-// const threadAddress = process.env.THREAD_IP_ADDRESS || "fd9b:3c63:6e6e:1:8cb6:83a1:5f52:967e";
-const threadAddress = "localhost";
+const threadAddress = process.env.THREAD_IP_ADDRESS;
 
 // Start the server
 server.listen(port, threadAddress, () => {
