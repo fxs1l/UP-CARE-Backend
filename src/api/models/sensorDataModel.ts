50de03150f58a4ee71d81713b5dd1aa04193f159
Asil Andrei Acasio <fx.silndrei@gmail.com>
2025-04-01 02:17:46 +0800

    fix: ensure local_time is int before invoking Date()

--- src/api/models/sensorDataModel.ts
+++ src/api/models/sensorDataModel.ts
@@ -24,7 +24,7 @@ const localDatabase = {
           .tag("sensor_model", sensorModel)
           .tag("parameter", parameter)
           .floatField("value", value)
-          .timestamp(new Date(local_time));
+          .timestamp(new Date(parseInt(local_time)));
         try {
           writeApi.writePoint(point);
         } catch (error) {
